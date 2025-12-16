ac5c4502e5ea87ce0e50c07fd47d044a670b8915
Kadalzz <gebychristy@gmail.com>
2025-12-16 10:36:07 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,8 +61,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <Header />
           <main className="flex-grow container mx-auto px-4 py-6">
             {children}
-            <Analytics />
-            <SpeedInsights />
           </main>
           <Footer />
         </ThemeProvider>
